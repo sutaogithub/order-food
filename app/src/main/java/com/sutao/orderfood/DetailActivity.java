83aee4a148a6713bec6667877c5ae5548ce3fb1b
zhangsutao <zhangsutao@cvte.com>
2017-03-14 14:06:23 +0800

    [feature] add detail page

---
+++ app/src/main/java/com/sutao/orderfood/DetailActivity.java
@@ -0,0 +1,23 @@
+package com.sutao.orderfood;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.sutao.base.BaseActivity;
+
+/**
+ * emailTo intern_zhangsutao@cvte.com
+ *
+ * @author zhangsutao
+ * @brief 简单的功能介绍
+ * @date 2017/3/13
+ */
+public class DetailActivity extends BaseActivity{
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
